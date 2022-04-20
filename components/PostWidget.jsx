54406b0b8c6e71258bdc490ace54a20f33024b3e
John Emerson <john.carv.sousa@gmail.com>
2022-04-20 18:41:19 -0300

    feat: import useState and useEffect in PostWidget

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 import Link from 'next/link'
 
-const PostWidget = () => {
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+    if(slug) {
+      
+    }
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div>PostWidget</div>
   )
