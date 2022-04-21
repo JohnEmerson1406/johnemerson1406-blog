187fc5ab6abdf0157445f9d139f5ff4895a82a1f
John Emerson <john.carv.sousa@gmail.com>
2022-04-21 17:26:32 -0300

    feat: add import getRecentPosts

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 import Link from 'next/link'
 
+import { getRecentPosts } from '../services'
+
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([])
     if(slug) {
