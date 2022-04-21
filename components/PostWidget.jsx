70e44717df6a3a6c34b8b9ea03f431a1518198bd
John Emerson <john.carv.sousa@gmail.com>
2022-04-21 17:39:44 -0300

    feat: show related posts

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -18,7 +18,11 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug])
 
   return (
-    <div>PostWidget</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        {slug ? 'Related Posts' : 'Recent Posts' }
+      </h3>
+    </div>
   )
 }
 
