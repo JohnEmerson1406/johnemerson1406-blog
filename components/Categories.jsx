13865fd77cbdd0145f785458a442ed846ec79d6b
John Emerson <john.carv.sousa@gmail.com>
2022-04-27 20:01:25 -0300

    feat: change categories view

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -11,7 +11,11 @@ const Categories = () => {
       .then((newCategories) => setCategories(newCategories))
   }, [])
   return (
-    <div>Categories</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8 pb-12'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        Categories
+      </h3>
+    </div>
   )
 }
 
