bc121433520210b853610ce51994228d30ef8871
John Emerson <john.carv.sousa@gmail.com>
2022-07-05 20:12:07 -0300

    feat: add image to postDetail

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const PostDetail = ({ post }) => {
   return (
     <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
-      <h1>PostDetail</h1>
+      <div className='relative overflow-hidden shadow-md mb-6'>
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className='object-top h-full w-full rounded-t-lg'
+        />
+      </div>
     </div>
   )
 }
