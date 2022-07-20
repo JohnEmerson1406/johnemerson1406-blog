7084291b54a9fc6ac583ac99ea9dfbbd93a8f80c
John Emerson <john.carv.sousa@gmail.com>
2022-07-20 18:45:30 -0300

    feat: create comments query

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -13,4 +13,14 @@ export default async function comments(req, res) {
       authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
     },
   })
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug } } }) { id }
+    }
+  `
+
+  const result = await graphQLClient.request(query, req.body)
+
+  return res.status(200).send(result)
 }
