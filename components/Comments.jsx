5445e5e21faea53c58db0f9aea710ad2a3a38864
John Emerson <john.carv.sousa@gmail.com>
2022-07-31 15:44:53 -0300

    feat: add comment.createdAt

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -22,6 +22,17 @@ const Comments = ({ slug }) => {
             {' '}
             Comments
           </h3>
+          {comments.map((comment) => (
+            <div key={comment.createdAt} className='border-b border-gray-100 mb-4 pb-4'>
+              <p className='mb-4'>
+                <span className='font-semibold'>{comment.name}</span>
+                {' '}
+                on
+                {' '}
+                {moment(comment.createdAt).format('MM DD, YYYY')}
+              </p>
+            </div>
+          ))}
         </div>
       )}
     </>
