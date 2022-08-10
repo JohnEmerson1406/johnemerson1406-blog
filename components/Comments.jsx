c577349c02e45e92f10c2b1c09446113c93f5528
John Emerson <john.carv.sousa@gmail.com>
2022-08-10 17:39:48 -0300

    refactor: change language to pt-BR

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -20,7 +20,7 @@ const Comments = ({ slug }) => {
           <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
             {comments.length}
             {' '}
-            Comments
+            {comments.length > 1 ? 'Comentários' : 'Comentário'}
           </h3>
           {comments.map((comment) => (
             <div key={comment.createdAt} className='border-b border-gray-100 mb-4 pb-4'>
