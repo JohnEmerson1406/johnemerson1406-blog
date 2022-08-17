23f39a9a1a247825803205d2f3e15ffbf2bcaa76
John Emerson <john.carv.sousa@gmail.com>
2022-08-17 19:58:32 -0300

    refactor: change date of postcard

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import moment from 'moment'
+import 'moment/locale/pt-br'
 import Link from 'next/link'
 
 const PostCard = ({ post }) => {
+  moment.locale('pt-br')
+
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-6 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
@@ -35,7 +38,7 @@ const PostCard = ({ post }) => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
           </svg>
           <span className="align-middle">
-            {moment(post.createdAt).format('MMM DD, YYYY')}
+            {moment(post.createdAt).format('DD MMM. YYYY')}
           </span>
         </div>
       </div>
