eadbf203e4cdbad6048318a4d719ec0e70c76914
John Emerson <john.carv.sousa@gmail.com>
2022-11-11 16:02:40 -0300

    Feat: add icon

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ posts }) {
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Johnggli Blog</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://avatars0.githubusercontent.com/u/43749971" type="image/x-icon" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
